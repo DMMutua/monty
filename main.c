4326e29ff9837c9038740e95f7eab5bf4db12c7d
DMMutua <davymtuerr@gmail.com>
2022-10-06 22:09:57 +0300

    Normal State

--- main.c
+++ main.c
@@ -1,47 +1,51 @@
 #include "monty.h"
-global_t header;
-/**
- * start_stack - declare and initialize header
- * @stack: Addres of stack
- */
-void start_stack(stack_t **stack)
-{
-	*stack = NULL;
-	header.first = stack;
-}
-/**
- * free_all - Free all asign malloc func
- */
-void free_all(void)
-{
-	stack_t *tmp1, *tmp2 = NULL;
 
-	tmp1 = *(header.first);
-	while (tmp1 != NULL)
-	{
-		tmp2 = tmp1->next;
-		free(tmp1);
-		tmp1 = tmp2;
-	}
-	free(header.buffer);
-	fclose(header.file);
-}
-
+int value[3] = {0, 0, 0};
 /**
- * main - num of arguments
- * @argc: number of arguments
- * @argv: arguments
- * Return: EXIT_FAILURE on Fail, EXIT_SUCCES on Succes
+ * main - Interpreter of the Monty Language
+ * @argc: argument count
+ * @argv: the list of arguments
+ * Return: 1 if passed
  */
+
 int main(int argc, char *argv[])
 {
-	stack_t *stack;
+	char *path, *line, *tok_line[2];
+	FILE *fp;
+	void (*fptr)(stack_t **stack, unsigned int ln);
+	stack_t *head;
+	size_t len, lineno, status;
+	ssize_t read;
 
-	start_stack(&stack);
-	if (argc == 2)
+	head = NULL;
+	line = NULL;
+	check_argc(argc);
+	path = argv[1];
+
+	fp = fopen(path, "r");
+	check_file_stream(fp, path);
+	for (lineno = 1; (read = getline(&line, &len, fp)) != -1; lineno++)
 	{
-		find_file(argv[1], &stack);
+		if (check_empty(line))
+			continue;
+		status = tokenize_line(line, tok_line);
+		if (status == 0)
+			continue;
+
+		check_if_push(tok_line, lineno);
+		check_fail(line, fp, head);
+		check_data_structure(tok_line[0]);
+		fptr = get_opcode_func(tok_line[0]);
+		check_opcode(fptr, lineno, tok_line[0]);
+		check_fail(line, fp, head);
+
+		(*fptr)(&head, lineno);
+		check_fail(line, fp, head);
+		clear_strings(tok_line);
 	}
-	fprintf(stderr, "USAGE: monty file\n");
-	exit(EXIT_FAILURE);
+	free(line);
+	fclose(fp);
+	free_stack(head);
+
+	return (0);
 }
